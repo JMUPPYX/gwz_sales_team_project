3a1ab891c32c9a0142c6f28d5ea33f062ee168ff
JMUPPYX <148784893+JMUPPYX@users.noreply.github.com>
2024-11-11 16:24:52 +0100

    sales daily

--- gwz_sales_daily.sql
+++ gwz_sales_daily.sql
@@ -1,6 +1,7 @@
 SELECT
     date_date AS day,
-    SUM(turnover) AS turnover
+    ROUND(SUM(turnover),2) AS turnover
+    ROUND(SUM(purchase_cost),2) AS purchase_cost
 FROM
     challenge1sql.cours14.gwz_sales
     day
